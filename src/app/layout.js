fd6972eb7964d241005eb11c26b0078aff2dda24
muzammil-linuxmint <muzammilibrahim13@gmail.com>
2025-01-27 09:48:54 +0530

    Add Vercel Analytics and Speed Insights dependencies and integrate into layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,6 @@
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -19,6 +22,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Analytics/>
+      <SpeedInsights/>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
